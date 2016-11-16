57df5ea4bb37d64115073f017b79d316cb9e3a51
Max Schoening <max@max.wtf>
2016-11-16 10:20:07 -0800

    Use non .canvas endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const got = require('got');
 function usecanvas(canvasURL) {
   return new Promise((resolve, reject) => {
     got(canvasURL, { json: true, retries: 1, timeout: 15000 })
-      .then(response => resolve(response.body))
+      .then(response => resolve(response.body.data.attributes))
       .catch(error => reject(error));
   });
 }
